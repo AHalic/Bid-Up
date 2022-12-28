c304d30021b1d21416e96924d27ae47c440f0909
AHalic <sophie.gama@aluno.ufes.br>
2022-12-28 00:14:21 -0300

    :recycle: :truck: adds functions + changes direct

--- contract/Auction.sol
+++ contract/Auction.sol
@@ -1,52 +1,83 @@
-pragma solidity ^0.6.0;
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.0;
 
 // Define the Auction contract
 contract Auction {
     // Store the current highest bid and bidder
     uint public highestBid;
     address public highestBidder;
+    // Stores all address who made a bid
+    address[] public bidders;
     // Store the address of the seller and the start and end times of the auction
     address public seller;
-    uint public startTime;
     uint public endTime;
     // Store the name of the product being auctioned
     string public productName;
     // Store the image data as a base64-encoded string
     string public imageData;
+    bool public close = false;
+
+    constructor (address _seller, uint _initialBid, uint _endTime, string memory _productName, string memory _imageData) {
+        highestBid = _initialBid;
+        seller = _seller;
+        endTime = _endTime;
+        productName = _productName;
+        imageData = _imageData;
+    }
 
     // Define an event for when a new bid is placed
     event NewBid(address bidder, uint value);
     // Define an event for when the auction is closed
     event AuctionClosed(uint finalBid, address winner);
 
-    // Define a function to place a new bid
-    function bid(uint value) public {
+    modifier bidAutorize (uint value) {
         // Check that the value of the new bid is higher than the current highest bid
         require(value > highestBid);
+
         // Check that the auction is still open
         require(block.timestamp < endTime);
+        _;
+    }
+
+    // Define a function to place a new bid
+    function bid(uint value) public bidAutorize (value) {
         // Update the highest bid and bidder
         highestBid = value;
         highestBidder = msg.sender;
+        bidders.push(msg.sender);
+
         // Trigger the NewBid event
         emit NewBid(msg.sender, value);
     }
 
-    // Define a function to close the auction and finalize the results
-    function closeAuction() public {
+    modifier closeAutorize {
         // Check that the caller is the seller
         require(msg.sender == seller);
-        // Transfer the winning bid to the seller
-        highestBidder.transfer(highestBid);
-        // Trigger the AuctionClosed event
-        emit AuctionClosed(highestBid, highestBidder);
-        // Set the highest bid and bidder to zero to indicate that the auction is closed
-        highestBid = 0;
-        highestBidder = address(0);
+        _;
     }
 
-    // Define a function to retrieve the deployer of the contract
-    function deployer() public view returns (address) {
-        return msg.sender;
+    // Define a function to close the auction and finalize the results
+    function closeAuction() public closeAutorize {
+        // Transfer the winning bid to the seller
+        payable(seller).transfer(highestBid);
+        // Trigger the AuctionClosed event
+        emit AuctionClosed(highestBid, highestBidder);
+
+        close = true;
+    }
+
+    function addrBid(address adr) public view returns (bool) {
+        for (uint i = 0; i < bidders.length; i++) {
+            if (bidders[i] == adr) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    function getClose() public view returns (bool) {
+        return close;
     }
 }
--- contract/AuctionFactory.sol
+++ contract/AuctionFactory.sol
@@ -1,6 +1,8 @@
-pragma solidity ^0.6.0;
+// SPDX-License-Identifier: GPL-3.0
 
-import "Auction.sol";
+pragma solidity ^0.8.0;
+
+import "./Auction.sol";
 
 // Define the AuctionFactory contract
 contract AuctionFactory {
@@ -10,12 +12,51 @@ contract AuctionFactory {
     // Define a function to create a new instance of the Auction contract
     function createAuction(
         address _seller,
+        uint _initialBid,
         string memory _productName,
-        uint _startTime,
         uint _endTime,
         string memory _imageData
     ) public {
         // Deploy a new instance of the Auction contract and store it in the array
-        deployedAuctions.push(new Auction(_seller, _startTime, _endTime, _productName, _imageData));
+        deployedAuctions.push(new Auction(_seller, _initialBid, _endTime, _productName, _imageData));
+    }
+
+
+    // LISTING ALL INTERACTIONS OF AN USER
+
+    // Define a function to retrieve the list of auctions that a user has participated in
+    function getUserHistory(address _user) public view returns (Auction[] memory) {
+        uint size = 0;
+
+        // Iterate over all contract instances of the Auction contract
+        for (uint i = 0; i < deployedAuctions.length; i++) {
+            // Retrieve the information for the current auction
+            Auction auction = deployedAuctions[i];
+
+            // Check if the user has participated in the current auction
+            if (auction.addrBid(_user)) {
+                size += 1;
+            }
+        }
+
+        uint j = 0;
+
+        // Create an array to store the auctions that the user has participated in
+        Auction[] memory userHistory = new Auction[](size);
+
+        for (uint i = 0; i < deployedAuctions.length; i++) {
+            Auction auction = deployedAuctions[i];
+
+            if (auction.addrBid(_user)) {
+                userHistory[j] = auction;
+                j += 1;
+            }
+        }
+        // Return the array of auctions that the user has participated in
+        return userHistory;
+    }
+
+    function getAuctions() public view returns (Auction[] memory) {
+        return deployedAuctions;
     }
 }
