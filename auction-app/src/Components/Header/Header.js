a32ef61b03b7c2579ec4161e60b449acbd5acbb9
AHalic <sophie.gama@aluno.ufes.br>
2022-12-29 01:09:34 -0300

    :sparkles: :construction: adds connection to metamask + get auctions

--- auction-app/src/Components/Header/Header.js
+++ auction-app/src/Components/Header/Header.js
@@ -7,43 +7,78 @@ import { Link, useNavigate } from 'react-router-dom';
 import { ethers } from "ethers";
 
 import { loginMetaMask } from '../../Services/metamask'
+import auction from "../../Services/keys/auctionKeys";
 
 import "./Header.css";
 import logo from "../../Assets/Logo.svg";
-import { RxMagnifyingGlass } from "react-icons/rx";
-import { IoMdArrowDropdown } from "react-icons/io"
+import { RxLightningBolt, RxMagnifyingGlass } from "react-icons/rx";
+import { IoMdArrowDropdown } from "react-icons/io";
 import { CiLogin } from "react-icons/ci";
 
-export default function Header({boolSearch, searchQuery, setSearchQuery}) {
+export default function Header({boolSearch, data, setData}) {
     const [search, setSearch] = useState("");
+    const [signer, setSigner] = useState("");
     const [auctFactory, setAuctFactory] = useState("");
 
     const navigate = useNavigate()
 
     // Search bar functions
-    const onSubmit = e => {
-        setSearchQuery(search)
+    const onSubmit = async (e) => {
         e.preventDefault()
         navigate(`?s=${search}`)
+
+        if (auctFactory) {
+            const localData = await auctFactory.getAuctions()
+
+            var newData = []
+            localData.map(async (address) => {
+                const auctionContract = new ethers.Contract(address, auction.abi, signer)
+
+                const productName = await auctionContract.productName()
+
+                if (productName.includes(search))
+                    newData.push(address)
+            })
+            setData(newData)
+
+        } else {
+            setData(false)
+        }
     };
     const handleSearchQueryChange = e => {
         setSearch(e.target.value)
     };
 
+    // Login to metamask
     const connectHandler = async () => {
         try {
-            await loginMetaMask(setAuctFactory)
+            await loginMetaMask(setAuctFactory, setSigner)
         } catch (error) {
             console.log('Something went wront: ', error);
         }
 
     };
+
+    // Get array of auctions
+    const getAuctions = async () => {
+        if (auctFactory) {
+            const data = await auctFactory.getAuctions()
+
+            setData(data)
+        } else {
+            setData(false)
+        }
+    }
+
+    useEffect(() => {
+        getAuctions()
+    }, [auctFactory])
     
 
     return (
         <div className="header">
             {/* Logo */}
-            <Link to="/">
+            <Link to="/" onClick={getAuctions}>
                 <img src={logo} className="logo" alt="Bid Up Logo"/>
             </Link>
             
@@ -66,6 +101,7 @@ export default function Header({boolSearch, searchQuery, setSearchQuery}) {
                     <p className="loginText">LOGIN</p>
                 </button>
             :
+            // Profile button
                 <button className="profileBtn">
                     <p className="profileBtnText">
                         PROFILE
