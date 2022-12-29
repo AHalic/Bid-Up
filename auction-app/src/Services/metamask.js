32f1b5c776937a189672e417a049f1b5089b01dc
AHalic <sophie.gama@aluno.ufes.br>
2022-12-29 01:10:04 -0300

    :recycle: set signer

--- auction-app/src/Services/metamask.js
+++ auction-app/src/Services/metamask.js
@@ -1,14 +1,15 @@
 import { ethers } from "ethers";
-import auction from "./keys/auctionKeys";
+import auction from "./keys/auctionFactKeys";
 
-export async function loginMetaMask(setAuctFactoryContract) {
+export async function loginMetaMask(setAuctFactoryContract, setSigner) {
 
     const provider = new ethers.providers.Web3Provider(window.ethereum, "goerli");
 
     provider.send("eth_requestAccounts", []).then(() => {
         provider.listAccounts().then((accounts) => {
         const signer = provider.getSigner(accounts[0]);
-        
+        setSigner(signer)
+
         const auctionContract = new ethers.Contract(auction.address, auction.abi, signer)
         setAuctFactoryContract(auctionContract)
 
