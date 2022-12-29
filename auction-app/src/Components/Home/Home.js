06899177d7099c2c6c58030b217f5bf49bf0b7a8
AHalic <sophie.gama@aluno.ufes.br>
2022-12-29 12:57:30 -0300

    :recycle: :construction: adds content div

--- auction-app/src/Components/Home/Home.js
+++ auction-app/src/Components/Home/Home.js
@@ -1,25 +1,33 @@
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from 'react-router-dom';
 
+import './Home.css';
 import Header from "./../Header/Header";
 
 export default function Home() {
     const [data, setData] = useState("")
 
     return (
-        <div>
+        <div className="homeOuter">
             <Header boolSearch={true} data={data} setData={setData}/>
-            <div>
+            {/* <div> */}
                 <div className="homeContent">
                     {data ? 
-                        data.map(auct => {
+                        <div className="innerContent">
+                            <p className="titleHome">OPEN AUCTIONS</p>
+                            {data.length > 0 ? 
+                                data.map(auct => {
 
-                        })
+                                }) 
+                            :
+                                <p className="textNoAuct">No Auctions avaible</p>
+                            }
+                        </div>
                     :
-                        <h1>Login to start bidding!</h1>
+                        <p className="titleHome">Login to start bidding!</p>
                     }
                 </div>
-            </div>
+            {/* </div> */}
         </div>
     );
 }
