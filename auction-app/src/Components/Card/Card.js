537670ab66352a6287cd55d3590db6e8bd883d05
AHalic <sophie.gama@aluno.ufes.br>
2022-12-30 18:49:14 -0300

    :sparkles: adds product navigation

--- auction-app/src/Components/Card/Card.js
+++ auction-app/src/Components/Card/Card.js
@@ -1,15 +1,19 @@
 import React, { useState, useEffect } from "react";
 import { ethers } from "ethers";
+import { useNavigate } from 'react-router-dom';
 
 import './Card.css'
 import auction from "../../Services/keys/auctionKeys";
 
-export default function Card({auctionAddress, signer}) {
+export default function Card({auctionAddress, signer, setSigner, auctFactory, setAuctFactory}) {
     const [name, setName] = useState("")
     const [price, setPrice] = useState("")
     const [image, setImage] = useState("")
     const [deadline, setDeadline] = useState("")
+    const [close, setClose] = useState(false)
     const [auctContract, setAuctContract] = useState("")
+
+    const navigate = useNavigate()
 
     useEffect(() => {
         const getData = async () => {
@@ -25,6 +29,9 @@ export default function Card({auctionAddress, signer}) {
             const auctImage = await auctionContract.imageData()
             setImage(auctImage)
 
+            const auctClose = await auctionContract.close()
+            setClose(auctClose)
+
             const auctEndDate = await auctionContract.endTime()
             const endDate = new Date(Number(auctEndDate))
             
@@ -37,7 +44,7 @@ export default function Card({auctionAddress, signer}) {
     }, [])
 
     function handleClick() {
-        
+        navigate(`/product/${auctionAddress}`)
     }
 
     return (
@@ -48,7 +55,11 @@ export default function Card({auctionAddress, signer}) {
             <div className="cardInfo">
                 <p className="cardTitle">{name}</p>
                 <p className="cardPrice">{`US $${price}`}</p>
-                <p className="cardTimeLeft">{deadline > 0 ? `${Math.floor(deadline/(1000*60*60*24))}d ${Math.floor((deadline/(1000*60*60))%24)}h` : 'To be closed'}</p>
+                {close ? 
+                    <p className="cardTimeLeft">Closed</p> 
+                : 
+                    <p className="cardTimeLeft">{deadline > 0 ? `${Math.floor(deadline/(1000*60*60*24))}d ${Math.floor((deadline/(1000*60*60))%24)}h` : 'To be closed'}</p>
+                }
             </div>
         </div>
     )
