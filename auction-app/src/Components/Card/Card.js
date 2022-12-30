99a9018e07df25a9da3eceec92554465d48a7f3c
AHalic <sophie.gama@aluno.ufes.br>
2022-12-29 23:58:27 -0300

    :sparkles: adds card data

--- auction-app/src/Components/Card/Card.js
+++ auction-app/src/Components/Card/Card.js
@@ -1,19 +1,48 @@
 import React, { useState, useEffect } from "react";
 import { ethers } from "ethers";
 
-export default function Card({auctionAddress}) {
-    const [image, setImage] = useState("")
+import './Card.css'
+import auction from "../../Services/keys/auctionKeys";
+
+export default function Card({auctionAddress, signer}) {
     const [name, setName] = useState("")
     const [price, setPrice] = useState("")
+    const [image, setImage] = useState("")
     const [deadline, setDeadline] = useState("")
 
     useEffect(() => {
+        const getData = async () => {
+            const auctionContract = new ethers.Contract(auctionAddress, auction.abi, signer)
+
+            const auctName = await auctionContract.productName()
+            setName(auctName)
+
+            const auctPrice = await auctionContract.highestBid()
+            setPrice(auctPrice)
+
+            const auctImage = await auctionContract.imageData()
+            setImage(auctImage)
+
+            const auctEndDate = await auctionContract.endTime()
+            const endDate = new Date(auctEndDate)
+            const today = new Date()
+            setDeadline(endDate - today)
+        }
+        getData()
         
-    }, [auctionAddress])
+    }, [])
 
     return (
         <div className="card">
-
+            <div className="cardImageDiv">
+                <img className="cardImage" src={image} alt={`${name}`}/>
+            </div>
+            <div className="cardInfo">
+                <p className="cardTitle">{name}</p>
+                <p className="cardPrice">{`US $${price}`}</p>
+                {/* <p className="cardTimeLeft">{`${deadline/1000*60*60*24}d ${(deadline/1000*60*60)%24}h`}</p> */}
+                <p className="cardTimeLeft">1d 3h</p>
+            </div>
         </div>
     )
 }
