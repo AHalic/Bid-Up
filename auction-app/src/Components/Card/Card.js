4b24f88f4b43b2ab34bc168c23736337528cbd23
AHalic <sophie.gama@aluno.ufes.br>
2023-01-01 16:24:17 -0300

    :recycle: adds my bids info

--- auction-app/src/Components/Card/Card.js
+++ auction-app/src/Components/Card/Card.js
@@ -5,12 +5,14 @@ import { useNavigate } from 'react-router-dom';
 import './Card.css'
 import auction from "../../Services/keys/auctionKeys";
 
-export default function Card({auctionAddress, signer}) {
+export default function Card({auctionAddress, signer, isMyBid}) {
     const [name, setName] = useState("")
     const [price, setPrice] = useState("")
     const [image, setImage] = useState("")
     const [deadline, setDeadline] = useState("")
+    const [closeDate, setCloseDate] = useState("")
     const [close, setClose] = useState(false)
+    const [highestBidder, setHighestBidder] = useState("")
 
     const navigate = useNavigate()
 
@@ -30,12 +32,17 @@ export default function Card({auctionAddress, signer}) {
             const auctClose = await auctionContract.close()
             setClose(auctClose)
 
+            var closeDate = await auctionContract.closeDate()
+            closeDate = new Date(Number(closeDate))
+            setCloseDate(closeDate)
+
             const auctEndDate = await auctionContract.endTime()
             const endDate = new Date(Number(auctEndDate))
-            
             const today = new Date()
-
             setDeadline(endDate - today)
+
+            const bidder = await auctionContract.highestBidder()
+            setHighestBidder(bidder)
         }
         getData()
 
@@ -46,15 +53,16 @@ export default function Card({auctionAddress, signer}) {
     }
 
     return (
-        <div className="card" onClick={handleClick}>
+        <div className={`card ${close ? 'cardPay' : ''}`} onClick={handleClick}>
             <div className="cardImageDiv">
                 <img className="cardImage" src={image} alt={`${name}`}/>
             </div>
             <div className="cardInfo">
                 <p className="cardTitle">{name}</p>
-                <p className="cardPrice">{`US $${price}`}</p>
+                <p className={`cardPrice ${isMyBid && highestBidder != signer ? 'cardPriceNot': 'cardPriceMy'}'}`}
+                >{`US $${price} ${isMyBid && highestBidder == signer ? ' not yours!': ''}`}</p>
                 {close ? 
-                    <p className="cardTimeLeft">Closed</p> 
+                    <p className="cardTimeLeft">{`Closed on ${closeDate.toLocaleDateString("en-US")}`}</p> 
                 : 
                     <p className="cardTimeLeft">{deadline > 0 ? `${Math.floor(deadline/(1000*60*60*24))}d ${Math.floor((deadline/(1000*60*60))%24)}h` : 'To be closed'}</p>
                 }
