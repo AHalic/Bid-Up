8963fb6775691c7992d5bd8f0260696af69cd826
AHalic <sophie.gama@aluno.ufes.br>
2023-01-10 22:47:15 -0300

    :poop: :construction: adds auction funct (IMAGE NOT WORKING)

--- auction-app/src/Pages/Bids/Bids.js
+++ auction-app/src/Pages/Bids/Bids.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { ethers } from "ethers";
 import { style, StyledTextField } from './style';
+import axios from "axios";
+import swal from "sweetalert";
+import { useNavigate } from "react-router-dom";
 
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
@@ -15,16 +18,75 @@ import Card from "../../Components/Card/Card";
 import auction from "../../Services/keys/auctionKeys";
 import Dropzone from "../../Components/Dropzone/Dropzone";
 
+const IMAGE_API_KEY="fce1701c294db35869bacc3eae6cd82f";
 
-function ButtonAdd ({setSelectedFile, date, setDate}) {
+function ButtonAdd ({signer, auctFactory}) {
     const [open, setOpen] = React.useState(false);
+    const [selectedFile, setSelectedFile] = useState(null)
+    const [deadline, setDeadline] = React.useState(Date());
+    const [title, setTitle] = useState("")
+    const [initialBid, setInitialBid] = useState(0)
 
-    const handleChange = (newValue) => {
-      setDate(newValue);
+    const navigate = useNavigate()
+
+    const handleDeadline = (newValue) => {
+      setDeadline(newValue);
     };
   
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
+    const handleTitle = (event) => setTitle(event.target.value);
+    const handleInitialBid = (event) => setInitialBid(event.target.value);
+
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+        const today = (new Date()).getTime()
+        const dateEnd = (new Date(deadline)).getTime()
+
+        const reader = new FileReader();
+
+        reader.onload = async () => {
+            await axios.post(
+                `https://api.imgbb.com/1/upload?key=${IMAGE_API_KEY}&image=${reader.result}`)
+                .then((res) => console.log(res)) // Handling success
+                .catch((err) => console.log(err)); // Handling error
+        }
+      
+        reader.readAsBinaryString(selectedFile);
+
+
+        console.log(selectedFile);
+        const url = URL.createObjectURL(selectedFile)
+
+        Promise.all([
+            auctFactory.createAuction(signer._address, String(initialBid), title, today, dateEnd, String(url))
+        ]).then(() => {
+            swal({
+                title: 'Your auction is in process, please wait',
+                icon: 'info',
+                button: false,
+                closeOnClickOutside: false,
+            })
+            auctFactory.on("AuctionCreated", (from, auction, name, createdAt) => {
+                swal({
+                    title: `Your auction is created! ${name} is ready to receive bids`,
+                    icon: 'success',
+                }).then(() => {
+                    handleClose()
+                    navigate('/auctions')
+                })
+            })
+        }).catch((error) => {
+            console.log('Something went wrong')
+            console.log(error?.error?.message)
+            swal({
+                title: `Something went wrong: ${error?.error?.message}}`,
+                icon: 'error',
+            })
+        })
+
+    }
 
     return (
         <div>
@@ -37,24 +99,26 @@ function ButtonAdd ({setSelectedFile, date, setDate}) {
                 aria-describedby="modal-modal-description"
             >
                 <Box style={style.box}>
-                    <form className="formModal">
+                    <form className="formModal" onSubmit={handleSubmit}>
                         <div className="leftColumn">
                             <Dropzone onFileUploaded={setSelectedFile}/>
                             <Button type='submit' style={style.send}>SEND</Button>
                         </div>
                         <div className="rightColumn">
-                            <StyledTextField id="outlined-basic" label="Product title" variant="outlined" size='small'/>
-                            {/* <StyledTextField id="outlined-basic" label="Final date" variant="outlined" size='small'/> */}
+                            <StyledTextField id="outlined-basic" label="Product title" variant="outlined" 
+                                size='small' onChange={handleTitle} required/>
                             <LocalizationProvider dateAdapter={AdapterDayjs}>
                                 <DesktopDatePicker
-                                    label="Deadline"
+                                    label="Deadline *"
                                     inputFormat="MM/DD/YYYY"
-                                    value={date}
-                                    onChange={handleChange}
+                                    value={deadline}
+                                    onChange={handleDeadline}
                                     renderInput={(params) => <StyledTextField {...params} />}
+                                    required
                                     />
                             </LocalizationProvider>
-                            <StyledTextField id="outlined-basic" label="Initial bid" variant="outlined" size='small' inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}/>
+                            <StyledTextField id="outlined-basic" label="Initial bid" variant="outlined" size='small' 
+                                inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }} onChange={handleInitialBid} required/>
                         </div>
                     </form>
                 </Box>
@@ -66,8 +130,6 @@ function ButtonAdd ({setSelectedFile, date, setDate}) {
 export default function Bids({signer, setSigner, auctFactory, setAuctFactory, auctions, isBid, title1, title2}) {
     const [openAuct, setOpenAuct] = useState("")
     const [closeAuct, setCloseAuct] = useState("")
-    const [selectedFile, setSelectedFile] = useState(null)
-    const [date, setDate] = React.useState(Date());
 
     useEffect(() => {
         // Get array of auctions    
@@ -135,7 +197,7 @@ export default function Bids({signer, setSigner, auctFactory, setAuctFactory, au
         }
     }, [auctFactory, auctions, isBid, signer])
 
-    
+
     return (
         <div className="homeOuter">
             <Header boolSearch={false} setSigner={setSigner} auctFactory={auctFactory} 
@@ -146,7 +208,7 @@ export default function Bids({signer, setSigner, auctFactory, setAuctFactory, au
                         <div className="innerContent">
                             <div className="titleBtn">
                                 <p className="titleHome">{title1}</p> 
-                                <ButtonAdd setSelectedFile={setSelectedFile} date={date} setDate={setDate} />
+                                <ButtonAdd signer={signer} auctFactory={auctFactory} />
                             </div>
                             <div className="cardsContainer">
                                 {
@@ -162,7 +224,7 @@ export default function Bids({signer, setSigner, auctFactory, setAuctFactory, au
                             {closeAuct.length === 0 ? 
                                 <div className="titleBtn">
                                     <p className="titleHome">{title2}</p> 
-                                    <ButtonAdd />
+                                    <ButtonAdd signer={signer} auctFactory={auctFactory} />
                                 </div>
                             :
                                 <p className="titleHome">{title2}</p>
@@ -180,7 +242,7 @@ export default function Bids({signer, setSigner, auctFactory, setAuctFactory, au
                     {closeAuct && openAuct && openAuct.length === 0 && closeAuct.length === 0 ?
                         <div className="titleBtn">
                             <p className="titleHome">{isBid ? 'You haven\'t bid yet' : 'You have no auctions yet'}</p>
-                            <ButtonAdd />
+                            <ButtonAdd signer={signer} auctFactory={auctFactory} />
                         </div>
                     : null}
                     {openAuct === false || closeAuct === false ? 
